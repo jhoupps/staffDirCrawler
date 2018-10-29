9801cb2e446a81addc0b5ab1743daf1de47299b9
jhoupps <j.c.houp7575@gmail.com>
2018-10-29 10:55:21 -0700

    Initial commit

---
+++ crawler.py
@@ -0,0 +1,5 @@
+#A web crawler for traversing the UW staff directory, located at http://www.washington.edu/home/peopledir/ 
+
+#A project by J. Houppermans, for an Informatics 300 Group Project
+
+print("testing 1,2,3... Hello world")
