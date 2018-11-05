0ee55a8c6bce05b360c6745a62c629dc49534381
jhoupps <j.c.houp7575@gmail.com>
2018-11-04 22:01:27 -0800

    added all-department functionality

--- scraper.py
+++ scraper.py
@@ -5,21 +5,46 @@
 import requests
 from bs4 import BeautifulSoup
 
-url = 'http://www.washington.edu/home/peopledir/'
 
-r = requests.post(url, data={'term': 'Information School', 'method': 'dept', 'whichdir': 'staff', 'length': 'full'})
+#Part one - getting the UW department Names
+
+url = 'http://www.washington.edu/about/academics/departments/'
+r = requests.get(url)
 
 soup = BeautifulSoup(r.text, 'html.parser')
+dep_names = soup.select("div.uw-body-copy li a")
 
-names = soup.select("div.contentcell h3")
-titles = soup.select("div.contentcell ul.multiaddr")
+output = []
+for dep_name in dep_names:
+    output.append(str(dep_name.string))
 
-for name, titles in zip(names, titles):
-    print(name.string)
-    jobs = titles.descendants
-    sketchy_work_around = ""
-    for job in jobs:
-        if str(job.string) != "None":
-            if str(job.string) != sketchy_work_around:
-                print(job.string)
-                sketchy_work_around = str(job.string)
+#Part two - method that sends requests to the staff directory 
+
+url2 = 'http://www.washington.edu/home/peopledir/'
+
+
+def searchDir(department):
+
+    r2 = requests.post(url2, data={'term': department, 'method': 'dept', 'whichdir': 'staff', 'length': 'full'})
+    
+    soup = BeautifulSoup(r2.text, 'html.parser')
+
+    names = soup.select("div.contentcell h3")
+    titles = soup.select("div.contentcell ul.multiaddr")
+
+    for name, titles in zip(names, titles):
+        print(name.string)
+        jobs = titles.descendants
+        sketchy_work_around = ""
+        for job in jobs:
+            if str(job.string) != "None":
+                if str(job.string) != sketchy_work_around:
+                    print(job.string)
+                    sketchy_work_around = str(job.string)
+                    
+#Part 3 - actually searching the directory
+
+for department_name in output:
+    searchDir(department_name)
+
+
