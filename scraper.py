089d459e7a4b35a61c5c0d321a919869bc1b3621
jhoupps <j.c.houp7575@gmail.com>
2018-11-26 09:56:17 -0800

    fixed major data flaw bug

--- scraper.py
+++ scraper.py
@@ -8,7 +8,13 @@ import csv
 
 with open('position_data.csv', 'w', newline='') as csvfile:
     filewriter = csv.writer(csvfile, delimiter=',', quotechar="'", quoting=csv.QUOTE_MINIMAL)
-    filewriter.writerow(['Name','Department Searched', 'Position', 'At'])
+    #colnames uses magic number for max amount of titles on a person observed
+    colnames = ["Name", "Department_Searched"]
+    for i in range(1, 9):
+        colnames.append("Position_"+str(i))
+        colnames.append("At_"+str(i))
+
+    filewriter.writerow(colnames)
 
     #Part 1 - getting the UW department Names
 
@@ -21,40 +27,71 @@ with open('position_data.csv', 'w', newline='') as csvfile:
     output = []
     for dep_name in dep_names:
         output.append(str(dep_name.string))
-
+    output = output[1:] #removes "home" from the department list
     #Part two - method that sends requests to the staff directory 
 
     url2 = 'http://www.washington.edu/home/peopledir/'
 
 
     def searchDir(department):
-
         r2 = requests.post(url2, data={'term': department, 'method': 'dept', 'whichdir': 'staff', 'length': 'full'})
         
         soup = BeautifulSoup(r2.text, 'html.parser')
 
         names = soup.select("div.contentcell h3")
-        titles = soup.select("div.contentcell ul.multiaddr")
+        #titles = soup.select("div.contentcell ul.multiaddr")
+        titles_listing = soup.select("ul.dir-listing")
 
-        for name, titles in zip(names, titles):
+        ##debug - complete rewrite that next section, snag algorithms from it as needed
+        #documentation note - titles_html_block is a bs4 tag
+
+        for name, titles_html_block in zip(names, titles_listing):
             clean_name = name.string.replace("'", "")
             clean_name = clean_name.replace("&apos;", "")
             clean_name = clean_name.replace(", MD", "")
             clean_name = clean_name.replace(",", "")
-            row_contents = [clean_name, department]
-            jobs = titles.descendants
-            sketchy_work_around = ""
-            for job in jobs:
-                if str(job.string) != "None":
-                    if str(job.string) != sketchy_work_around:
-                        #print(job.string)
-                        row_contents.append(job.string)
-                        sketchy_work_around = str(job.string)
-            filewriter.writerow(row_contents)
-                        
+
+            clean_department = department.replace(", ", " ̦")
+            
+            #debug progress - this prints every single name, and doesn't skip anyone
+            multi = titles_html_block.select("ul.multiaddr")
+            solo = titles_html_block.select("ul.title")
+
+            #len = 1 or len =0 depending on which it is. Does grab both
+
+            both_kinds_list = []
+            #people with the multiaddr tag typically (though not always)
+            #   have more than one title
+            #and have the format <li> content <br> </li>
+            #   except for the last element, which is <li> content </li>   
+            if(len(multi) > 0):
+                both_kinds_list = multi[0].select("li")
+                
+            #people with the solo tag typically (though not always)
+            #   have just one title
+            # and the format is <li> content </li>  
+            elif(len(solo) > 0):
+                both_kinds_list = solo[0].select("li")
+
+            row_contents = [clean_name, clean_department]
+
+            if(len(both_kinds_list) > 0):
+                for title in both_kinds_list:
+                    clean_title = title.get_text().replace(", Emeritus", "- Emeritus")
+                    job_and_department = clean_title.split(",")
+                                    
+                    ##might not actually be necessary, with the emeritus change
+                    clean_job = job_and_department[0].replace(",", " ̦")
+                    clean_job_department = job_and_department[1].replace(",", " ̦")
+
+                    row_contents.append(clean_job.strip())
+                    row_contents.append(clean_job_department.strip())
+                    
+                    filewriter.writerow(row_contents)
+
     #Part 3 - actually searching the directory
 
+#FLAG - DEBUG - DEACTIVATED MULTI-DEPARTMENT FUNCTIONALITY
     for department_name in output:
         searchDir(department_name)
-
 
